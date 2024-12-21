df1acaecf2ee6ca1ad409b4625f99ee28fdc9f99
Kakashi85sl <110409625+Kakashi85sl@users.noreply.github.com>
2024-12-21 17:00:27 +0100

    Add files via upload

--- front-end.js
+++ front-end.js
@@ -1,3 +1,5 @@
+// Active the burger menu and the off-screen menu
+
 const burgerMenu = document.querySelector(".menuBurger")
 
 const menu = document.querySelector(".off-screen")
@@ -6,3 +8,21 @@ burgerMenu.addEventListener("click", () => {
     burgerMenu.classList.toggle("active");
     menu.classList.toggle("active");
 })
+
+// Clean the form fields after submit or show an error message
+
+const send = document.querySelector("#submit")
+const errorMessage = document.querySelector(".error")
+const fill = document.querySelector("#email")
+const form = document.querySelector("#Form")
+
+send.addEventListener("click", () => {
+    event.preventDefault()
+    if (fill.value === "") {
+        errorMessage.textContent = "Please fill all the required fields"
+        return
+    }
+    errorMessage.textContent = "-- Your message has been sent! --";
+    form.reset()
+})
+
